caea00d10ae0f9f66779018cf765e3a8d50326d8
MinYoung Choe <30820487+young917@users.noreply.github.com>
2024-10-17 17:42:17 +0900

    Update and rename run_eval_ext_HyperK_email-Eu-full.sh to run_eval_ext_HyRec_email-Eu-full.sh

--- Property/run/run_eval_ext_HyperK_email-Eu-full.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-./bin/Evaluation --inputpath ./results/ext_HyperK/email-Eu-full/0/hypergraph --outputdir ./results/ext_HyperK/email-Eu-full/0/ --dupflag
-cd src
-python calculation_helper.py --inputpath ../results/ext_HyperK/email-Eu-full/0/hypergraph --outputdir ../results/ext_HyperK/email-Eu-full/0/ --sv --effdiam --dupflag --appxflag
-cd ../
